cd802dee3721c05a0883afaabe3dce0cf0168c7e
Henrique Lopes <henriquelopesls032@gmail.com>
2020-08-03 18:24:02 -0300

    att

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 
+
+import PageHeader from '../../components/PageHeader';
+import './styles.css';
+
+
 function TeacherList(){
     return(
-        <h1>Teacher</h1>
+        <div id="page-teacher-list" className="container">
+            <PageHeader />
+        </div>
     )
 }
 export default TeacherList
