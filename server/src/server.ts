0e95014d7a32b32ec5a52f45be7d47086623aa90
Henrique Lopes <henriquelopesls032@gmail.com>
2020-08-04 17:40:58 -0300

    update routes and server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
-
-app.get('/', (request, response) => {
- return response.json({ message: 'Hello word'})
-});
+app.use(routes);
 
 //Porta localhost:3333
 app.listen(3333);
