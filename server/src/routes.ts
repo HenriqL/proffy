5d4498aa65d375c18d8c35205fa5c885c222cb57
Henrique Lopes <henriquelopesls032@gmail.com>
2020-08-04 20:50:39 -0300

    update routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,14 @@
 import express from 'express';
 import database from './database/connection';
+import convertHourToMinutes from './utils/convertHourToMinutes';
 
 const routes = express.Router();
 
+interface ScheduleItem {
+    week_day: number;
+    from: string;
+    to: string;
+}
 
 
 
@@ -17,15 +23,46 @@ routes.post('/classes', async (request, response) => {
         schedule
     } = request.body;
 
-    await database('users').insert({
-        name,
-        avatar,
-        whatsapp,
-        bio,
-    })
+    const trx = await database.transaction();
 
-    return response.send();
+    try {
+        const insertedUserIds = await trx('users').insert({
+            name,
+            avatar,
+            whatsapp,
+            bio,
+        });
+        const user_id = insertedUserIds[0];
+     
+        const insertedClassesIds = await trx('classes').insert({
+            subject,
+            cost,
+            user_id,
+        })
+    
+    
+        const class_id = insertedClassesIds[0];
+    
+        const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
+            return {
+                class_id,
+                week_day: scheduleItem.week_day,
+                from: convertHourToMinutes(scheduleItem.from),
+                to: convertHourToMinutes(scheduleItem.to)
+            };
+        })
+    
+        await trx('class_schedule').insert(classSchedule)
+    
+        await trx.commit();
+        return response.status(201).send();
 
+    } catch (err) {
+        await trx.rollback();
+        return response.status(400).json({
+            error: 'Unexpected error white creating new class'
+        })
+    }
 });
 
 export default routes;
