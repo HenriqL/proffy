79698af7b7837d84306572f7ad6b84fff3ed9f6b
Henrique Lopes <henriquelopesls032@gmail.com>
2020-08-05 13:29:26 -0300

    create teacher form

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react';
-
+import './styles.css';
 
 import PageHeader from '../../components/PageHeader';
 
 function TeacherForm(){
     return(
         <div id="page-teacher-form" className="container">
-            <PageHeader title="Que incrivel que você quer dar aulas" />
+            <PageHeader title="Que incrivel que você quer dar aulas" 
+                description="O primeiro passo é preencher o formulario de inscrição"
+            />
+
+            <main>
+                <fieldset>
+                    <legend>Seus dados</legend>
+                    <div className="imput-block">
+                        <label htmlFor="name">Nome completo</label>
+                        <input type="text" id=""/>
+                    </div>
+
+                    <div className="imput-block">
+                        <label htmlFor="avatar">Avatar</label>
+                        <input type="text" id="avatar"/>
+                    </div>
+
+                    <div className="imput-block">
+                        <label htmlFor="whatsapp">Whatsapp</label>
+                        <input type="text" id="whatsapp"/>
+                    </div>
+                </fieldset>
+            </main>
+
+
+
+
+
         </div>
     )
 }
