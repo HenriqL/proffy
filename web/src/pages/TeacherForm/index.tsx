e35fc3ef602c53fa2082f3118a1a45919a2ca47d
Henrique Lopes <henriquelopesls032@gmail.com>
2020-08-05 16:23:40 -0300

    create select

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 import Textarea from '../../components/Textarea';
-
+import Select from '../../components/Select';
 import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css';
+
 
 function TeacherForm(){
     return(
@@ -19,13 +20,31 @@ function TeacherForm(){
                     <Input name="name" label="Nome completo"/>
                     <Input name="avatar" label="Avatar"/>
                     <Input name="whatsapp" label="Whatsapp"/>
+                    <Textarea name="bio" label="Biografia"/>
                 </fieldset>
 
                 <fieldset>
                     <legend>Sobre a aula</legend>
-                    <Input name="subject" label="Matéria"/>
+                    <Select 
+                        name="subject" 
+                        label="Matéria"
+                        options={[
+                            {value: 'Marketind Digital', label: 'Marketing Digital'},
+                            {value: 'Desenvolvimento web', label: 'Desenvolvimento web'},
+                            {value: 'Desenvolvimento de sistemas', label: 'Desenvolvimento de sistemas'},
+                            {value: 'Git e Github', label: 'Git e Github'},
+                            {value: 'Tipografia', label: 'Tipografia'},
+                            {value: 'Lettering', label: 'Lettering'},
+                            {value: 'Desing', label: 'Desing'},
+                        ]
+                        }
+                    />
+
+
+
+
                     <Input name="cost" label="Custo da sua hora por aula"/>
-                    <Textarea name="bio" label="Biografia"/>
+                   
                 </fieldset>
 
                 <footer>
