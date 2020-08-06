4f222e7dc71ad145a6bef44e0cdeb9ff057a70fc
Henrique Lopes <henriquelopesls032@gmail.com>
2020-08-06 09:47:33 -0300

    update

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -4,27 +4,38 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
 import  './styles.css';
 
-function TeacherItem (){
+export interface Teacher {
+        id: number;
+        avatar: string;
+        bio: string;
+        cost: number;
+        name: string;
+        subject: string;
+        whatsapp: string;
+}
+interface TeacherItemProps{
+    teacher: Teacher;
+}
+
+const TeacherItem: React.FunctionComponent<TeacherItemProps> = ({teacher}) =>{
     return(
         <article className="teacher-item">
         <header>
-            <img src="https://scontent.frec31-1.fna.fbcdn.net/v/t1.0-9/106665794_4008828212522036_7010429114915485257_n.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_ohc=TpyPnh_PMRkAX9KBx3M&_nc_ht=scontent.frec31-1.fna&oh=4bbcd8f2f913c05564fe6878e1c01a5b&oe=5F50C8A3" alt="Lavinia Dantas"/>
+            <img src={teacher.avatar} alt={teacher.name}/>
             <div>
-                <strong>Lavinia Dantas</strong>
-                <span>Marketing</span>
+                <strong> {teacher.name} </strong>
+                <span>{teacher.subject}</span>
             </div>
         </header>
 
         <p>
-            Te ensino a ser uma empreenderora de sucesso.
-            <br /> <br />
-            Em 90 dias te garanto sucesso e fama. Mas não garanto seu dinheiro de volta.
+        {teacher.bio}
         </p>
 
         <footer>
             <p>
                 Preço / Hora
-                <strong>R$ 100,00</strong>
+                <strong>R$ {teacher.cost}</strong>
             </p>
             <button type="button">
                 <img src={whatsappIcon} alt="whatsapp"/>
