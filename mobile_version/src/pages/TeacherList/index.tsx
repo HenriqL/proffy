e23e55a878c73a5b9565e540eadb9bb8d5c2ebc6
Henrique Lopes <henriquelopesls032@gmail.com>
2020-08-11 10:38:14 -0300

    Create Proffy page mobile

--- mobile_version/src/pages/TeacherList/index.tsx
+++ mobile_version/src/pages/TeacherList/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { View } from 'react-native';
 import PageHeader from '../../components/PageHeader';
+import TeacherItem from '../../components/TeacherItem';
+
 
 import styles from './styles'
+
 
 
  function TeacherList(){
      return (
          <View style={styles.container}>
             <PageHeader title="Proffys disponiveis"></PageHeader>
+         <TeacherItem></TeacherItem>
          </View>
+
      )
  }
  export default TeacherList;
